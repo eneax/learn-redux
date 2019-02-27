5762ccd23fdb2070ed5e93c5b05e050e6bb7a516
eneax <19818937+eneax@users.noreply.github.com>
2019-02-27 20:36:02 +0100

    Updating the State

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+// An action is an object which describes what sort of transformation you want to make to your state.
+{
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+    complete: false,
+  }
+}
+
+{
+  type: 'REMOVE_TODO',
+  id: 0,
+}
+
+{
+  type: 'TOGGLE_TODO',
+  id: 0,
+}
+
+{
+  type: 'ADD_GOAL',
+  goal: {
+    id: 0,
+    name: 'Run a Marathon'
+  }
+}
+
+{
+  type: 'REMOVE_GOAL',
+  id: 0
+}
+
+
 // 'createStore()' is function we'll invoke to create a new store
 function createStore() {
   // The store should have four parts
