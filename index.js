45495d6e76492811645b20eead3b240b84135906
eneax <19818937+eneax@users.noreply.github.com>
2019-02-28 14:41:49 +0100

    Combining Reducers

--- index.js
+++ index.js
@@ -1,60 +1,38 @@
-// List of Actions
-// An action is an object which describes what sort of transformation you want to make to your state.
-{
-  type: 'ADD_TODO',
-  todo: {
-    id: 0,
-    name: 'Learn Redux',
-    complete: false,
-  }
-}
-
-{
-  type: 'REMOVE_TODO',
-  id: 0,
-}
-
-{
-  type: 'TOGGLE_TODO',
-  id: 0,
-}
-
-{
-  type: 'ADD_GOAL',
-  goal: {
-    id: 0,
-    name: 'Run a Marathon'
-  }
-}
-
-{
-  type: 'REMOVE_GOAL',
-  id: 0
-}
-
-/*
-Characteristics of a Pure Function
-1) They always return the same result if the same arguments are passed in.
-2) They depend only on the arguments passed into them.
-3) Never produce any side effects.
-*/
-
-
 // Reducer function (takes state and action and reduces them to new state)
 // if state is undefined, set it to empty array
 function todos (state = [], action) {
   switch(action.type) {
-    case 'ADD_TODO' :
+    case 'ADD_TODO':
       return state.concat([action.todo])
-    case 'REMOVE_TODO' :
+    case 'REMOVE_TODO':
       return state.filter((todo) => todo.id !== action.id)
-    case 'TOGGLE_TODO' :
+    case 'TOGGLE_TODO':
       return state.map((todo) => todo.id !== action.id ? todo :
         Object.assign({}, todo, {complete: !todo.complete})
       )
-    default :
+    default:
       return state
+  }
 }
+
+function goals (state = [], action) {
+  switch(action.type) {
+    case 'ADD_GOAL':
+      return state.concat([action.goal])
+    case 'REMOVE_GOAL':
+      return state.filter((goal) => goal.id !== action.id)
+    default:
+      return state
+  }
+}
+
+function app (state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: goals(state.goals, action),
+  }
+}
+
 
 // 'createStore()' is function we'll invoke to create a new store
 function createStore(reducer) {
@@ -100,15 +78,72 @@ function createStore(reducer) {
 Internally, "createStore" gets the new state of the store by invoking "reducer", 
 passing it the current state and the action which was dispatched.
 */
-const store = createStore(todos);
+const store = createStore(app)
+
+store.subscribe(() => {
+  console.log('The new state is: ', store.getState())
+})
+
+
+// List of dispatches containing 'actions'
 store.dispatch({
   type: 'ADD_TODO',
   todo: {
     id: 0,
-    name: 'Learn Redux',
+    name: 'Walk the dog',
     complete: false,
   }
 })
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 1,
+    name: 'Wash the car',
+    complete: false,
+  }
+})
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 2,
+    name: 'Go to the gym',
+    complete: true,
+  }
+})
+
+store.dispatch({
+  type: 'REMOVE_TODO',
+  id: 1
+})
+
+store.dispatch({
+  type: 'TOGGLE_TODO',
+  id: 0
+})
+
+store.dispatch({
+  type: 'ADD_GOAL',
+  goal: {
+    id: 0,
+    name: 'Learn Redux'
+  }
+})
+
+store.dispatch({
+  type: 'ADD_GOAL',
+  goal: {
+    id: 1,
+    name: 'Lose 20 pounds'
+  }
+})
+
+store.dispatch({
+  type: 'REMOVE_GOAL',
+  id: 0
+})
+
 
 
 /* 
@@ -136,4 +171,14 @@ function todos (state = [], action) {
   }
 }
 
+*/
+
+
+// An action is an object which describes what sort of transformation you want to make to your state.
+
+/*
+  Characteristics of a Pure Function
+  1) They always return the same result if the same arguments are passed in.
+  2) They depend only on the arguments passed into them.
+  3) Never produce any side effects.
 */
